08b13ecac226b7a747043d237f5a7c82cf58b67c
samir-27 <sumrasamir27@gmail.com>
2023-10-09 13:04:01 +0530

    ref: refactor my cart

--- src/components/MyCart.tsx
+++ src/components/MyCart.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { PiHeartStraightThin } from 'react-icons/pi';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AiFillHeart } from 'react-icons/ai';
 
 const MyCart = () => {
   return (
     <div className="profile-page py-5">
-      <div className='d-flex row justify-content-center text-center'>
-        <PiHeartStraightThin className='text-secondary big-icon' /><br /><br />
-        <h1 >YOUR CART IS EMPTY</h1>
+      <div className="d-flex row justify-content-center text-center">
+        <AiFillHeart size={100} className="heart-fill"></AiFillHeart>
+        <br />
+        <br />
+        <h1>YOUR CART IS EMPTY</h1>
         <p>You don't have any products in your Cart</p>
-        <Link to={'/explore'}>
-          <Button className='my-4 profile-btn' >Go to Shop</Button>
-        </Link>
+        
+          <Button className="my-4 w-25 "><Link to={'/explore'} className='text-light'>Go to Shop</Link></Button>
+ 
       </div>
     </div>
   );
