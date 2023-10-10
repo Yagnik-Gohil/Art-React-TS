2fd94f5381d4d23e5c742b9b67500f373fe29dfd
yagnik <gohilyagnik3@gmail.com>
2023-10-10 22:59:54 +0530

    feat: added popup view for image

--- src/components/ExploreCard.tsx
+++ src/components/ExploreCard.tsx
@@ -1,24 +1,60 @@
 import { AiFillHeart } from 'react-icons/ai';
 import { FaEye } from 'react-icons/fa';
 import { BsCartPlusFill } from 'react-icons/bs';
+import Modal from 'react-bootstrap/Modal';
+import { Fragment, useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 function ExploreCard({ data }: any) {
   // console.log(data)
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div className="col-3 explore-card">
-      <div>
-        <img src={data.path} alt="product" className="explore-card-image"></img>
-        <div className="explore-card-icons-div">
-          <AiFillHeart size={20} className='heart-fill mx-2 explore-icon'></AiFillHeart>
-          <FaEye size={20} className='fill-black mx-2 explore-icon'></FaEye>
-          <BsCartPlusFill size={20} className='fill-black mx-2 explore-icon'></BsCartPlusFill>
+    <Fragment>
+      <div className="col-3 explore-card">
+        <div>
+          <img
+            src={data.path}
+            alt="product"
+            className="explore-card-image"
+          ></img>
+          <div className="explore-card-icons-div">
+            <AiFillHeart
+              size={20}
+              className="heart-fill mx-2 explore-icon"
+            ></AiFillHeart>
+            <FaEye
+              size={20}
+              className="fill-black mx-2 explore-icon"
+              onClick={handleShow}
+            ></FaEye>
+            <BsCartPlusFill
+              size={20}
+              className="fill-black mx-2 explore-icon"
+            ></BsCartPlusFill>
+          </div>
         </div>
+        <h5 className="m-0 pt-2">{data.title}</h5>
+        <p>
+          Price: {data.price} {data.currency}
+        </p>
       </div>
-      <h5 className="m-0 pt-2">{data.title}</h5>
-      <p>
-        Price: {data.price} {data.currency}
-      </p>
-    </div>
+
+      <Modal show={show} onHide={handleClose} centered className='d-flex justify-content-center p-0'>
+        <div>
+          <div className='model-image'>
+            <img src={data.path} alt="product"></img>
+          </div>
+          <div className='text-center p-1'>
+            <Button variant="light" size="sm" className='border mx-2'>ADD TO WISHLIST</Button>
+            <Button variant="light" size="sm" className='border mx-2'>VIEW DETAILS</Button>
+            <Button variant="light" size="sm" className='border mx-2'>ADD TO CART</Button>
+          </div>
+        </div>
+      </Modal>
+    </Fragment>
   );
 }
 
