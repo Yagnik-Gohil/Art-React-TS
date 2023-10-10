ea3bd6c3ace83b9a17bc41c791088d1964147b4c
yagnik <gohilyagnik3@gmail.com>
2023-10-10 23:03:01 +0530

    feat: image zoom feature

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -8,8 +8,9 @@ import { TbZoomIn, TbZoomOut, TbZoomReset } from 'react-icons/tb';
 function Product() {
   const params = useParams();
   const navigate = useNavigate();
-
+  
   const [product, setProduct] = useState<any>();
+  const [height, setHeight] = useState(100);
 
   const handleBack = () => {
     navigate(-1);
@@ -19,6 +20,29 @@ function Product() {
     const product = Images.find((o) => o.id === params.id);
     setProduct(product);
   }, []);
+
+
+  const handleZoomIn = () => {
+    console.log(height)
+    if (height < 150) { // Adjust the maximum zoom level as needed
+      console.log('first')
+      setHeight(height + 20);
+    }
+  };
+
+  const handleZoomOut = () => {
+    if (height > 50) { // Adjust the minimum zoom level as needed
+      setHeight(height - 20);
+    }
+  };
+
+  const handleZoomReset = () => {
+    setHeight(100);
+  };
+
+  const imageStyle = {
+    height: `${height}%`, // Apply zoom using CSS scale transform
+  };
 
   return (
     <Fragment>
@@ -44,22 +68,22 @@ function Product() {
       <div className="row zoom-row">
         <div className="col zoom-col">
           <div>
-            <div className='zoom-icon my-2 bg-light border border-secondary rounded-circle d-flex align-items-center justify-content-center'>
-              <TbZoomIn size={20} />
+            <div className="zoom-icon my-2 bg-light border border-secondary rounded-circle d-flex align-items-center justify-content-center">
+              <TbZoomIn size={20} onClick={handleZoomIn} />
             </div>
-            <div className='zoom-icon my-2 bg-light border border-secondary rounded-circle d-flex align-items-center justify-content-center'>
-              <TbZoomOut size={20} />
+            <div className="zoom-icon my-2 bg-light border border-secondary rounded-circle d-flex align-items-center justify-content-center">
+              <TbZoomOut size={20} onClick={handleZoomOut} />
             </div>
-            <div className='zoom-icon my-2 bg-light border border-secondary rounded-circle d-flex align-items-center justify-content-center'>
-              <TbZoomReset size={20} />
+            <div className="zoom-icon my-2 bg-light border border-secondary rounded-circle d-flex align-items-center justify-content-center">
+              <TbZoomReset size={20} onClick={handleZoomReset} />
             </div>
           </div>
         </div>
       </div>
       <div className="row product-details">
         <div className="col col-md-6 col-sm-6">
-          <div className="product-image">
-            <img src={product?.path} alt="product"></img>
+          <div className="bg-light rounded product-image d-flex align-items-center justify-content-center custom-scroll">
+            <img src={product?.path} alt="product" style={imageStyle}></img>
           </div>
         </div>
 
