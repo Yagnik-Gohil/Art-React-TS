fc4cf33efeedfcbe55aa5a00bc2707d2c4cde654
samir-27 <sumrasamir27@gmail.com>
2023-10-11 18:43:22 +0530

    feat: added Products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import Images from '../utils/category';
+import ProductImages from '../utils/product';
 import HomeCarousel from '../components/HomeCarousel';
 import HomeCard from '../components/HomeCard';
+import Products from '../components/Products'
 function Home() {
-
+  
 
   return (
     <>
       <HomeCarousel />
       <h1 className="text-center py-3">Explore By Catagory</h1>
-      <br></br>
+
       <div className="d-flex justify-content-center flex-wrap">
         {Images.map((data: any, index: number) => {
           return <HomeCard data={data} key={index}></HomeCard>;
         })}
       </div>
+      <h1 className="text-center py-3">Top Products</h1>
+      <div className="home-Products justify-content-center d-flex flex-wrap">
+        {ProductImages.map((data: any, index: number) => {
+          return <Products data={data} key={index}></Products>;
+        })}
+      </div>
+      <br></br>
+      {/* <h1 className='text-center py-3'>Art By Price</h1> */}
     </>
   );
 }
---
+++ src/utils/product.tsx
@@ -0,0 +1,35 @@
+const ProductImages = [
+    {
+      title: 'Sculpture',
+      text: '420 Sculpture',
+      path: 'https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'Serigraph',
+      text: '420 Sculpture',
+      path: 'https://images.unsplash.com/photo-1536924940846-227afb31e2a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'Photography',
+      text: '420 Sculpture',
+      path: 'https://images.unsplash.com/photo-1531913764164-f85c52e6e654?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHBhaW50aW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'Digital Art',
+      text: '420 Sculpture',
+      path: 'https://images.unsplash.com/photo-1578926375605-eaf7559b1458?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBhaW50aW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'Sculpture',
+      text: '420 Sculpture',
+      path: 'https://images.unsplash.com/photo-1578301978018-3005759f48f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fGFydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+        title: 'Printmaking',
+        text: '420 Sculpture',
+        path: 'https://images.unsplash.com/photo-1579783928621-7a13d66a62d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHBhaW50aW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+      },
+  ];
+  
+  export default ProductImages;
+  
