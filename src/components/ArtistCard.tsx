265faf8cc0e3abeacf80558c8e11188e0b53599c
samir-27 <sumrasamir27@gmail.com>
2023-10-31 18:43:03 +0530

    feat: added icons

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -1,10 +1,14 @@
 import { Fragment, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import { BsCartPlusFill } from 'react-icons/bs';
+import { FaEye } from 'react-icons/fa';
 function ArtistCard({ data }: any) {
   // console.log(data)
 
   const navigate = useNavigate();
-
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
   const handleClick = () => {
     navigate(`/ArtistPage/${data.id}`);
   };
@@ -18,6 +22,14 @@ function ArtistCard({ data }: any) {
             className="explore-card-image"
             onClick={handleClick}
           ></img>
+          <div className="explore-card-icons-div d-flex justify-content-center">
+            <div className="d-flex align-items-start">
+
+            <img className='facebook mx-2 explore-icon' src='https://cdn-icons-png.flaticon.com/128/3670/3670032.png'></img>
+                <img className='instagram mx-2 explore-icon' src='https://cdn-icons-png.flaticon.com/128/3955/3955024.png'></img>
+
+            </div>
+          </div>
         </div>
         <h4 className='mt-2'>
           {data.name}
